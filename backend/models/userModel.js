83d9d8bb8bbff9174c4124bf9c3b998f853e33b5
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-06-16 17:04:44 +0530

    user and doctor model

---
+++ backend/models/userModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type:String, 
+        required:true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+        type:String,
+        required: true
+    },
+
+    image:{
+        type:String,
+        default: "https://media.istockphoto.com/id/1478688327/vector/user-symbol-account-symbol-vector.jpg?s=612x612&w=0&k=20&c=N1Wxw0XjkUoXT9_Vaxa4SNIj1IvdJ2L2GQfEVVMTaFM="
+    },
+
+    address:{
+        type: Object,
+        default:{
+            line1:"",
+            line2:""
+        }
+    },
+
+    date:{
+        type:Number,
+        required:true
+    },
+
+    gender:{
+        type: String, 
+        default:"not selected"
+    },
+    
+    dob:{
+        type: String, 
+        default:"not selected"
+    },
+
+    phone:{
+        type:string,
+        default:"000000000"
+    },
+
+    slots_booked:{
+        type:Object,
+        default:{}
+    }
+
+})  //so tht we can use empty object as default value 
+
+const userModel = mongoose.models.user || mongoose.model('user',userSchema)
+
+export default userModel
+
+
