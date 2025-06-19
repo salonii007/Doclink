d6244b435cfc0de22246b40cd8db851b2a4096bc
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-06-20 00:52:49 +0530

    Sidemenu in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import SideMenu from './components/SideMenu';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
+import { Routes,Route } from 'react-router-dom';
  
 
 const App = () => {
@@ -13,6 +19,17 @@ const App = () => {
   return aToken ? (
     <div className='bg-[#F8F9FD]'>
       <Navbar/>
+      <div className='flex items-start'>
+        <SideMenu/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+           <Route path='/admin-dashboard' element={<Dashboard/>}/>
+            <Route path='/all-appointments' element={<AllAppointments/>}/>
+         <Route path='/add-doctor' element={<AddDoctor/>}/>
+        <Route path='/doctor-list' element={<DoctorsList/>}/>
+        </Routes>
+        
+      </div>
       
      
     </div>
