bd4a229dd325f26bd31eb964243527c44763e2e1
Saloni Mahajan <saloni.mahajan99@gmail.com>
2025-07-03 01:16:37 +0530

    Doctor dashboard, get profile, edit profile api

--- backend/controllers/doctorcontroller.js
+++ backend/controllers/doctorcontroller.js
@@ -116,4 +116,70 @@ const appointmentCancel = async(req, res)=>{
    } 
 }
 
-export {changeAvailablity, appointmentsDoctor, loginDoctor, doctorList, appointmentCancel, appointmentComplete}
+
+//API to get dashboard data for doctor panel
+
+const doctorDashboard = async(req, res)=>{
+    try{
+        const {docId}= req.body
+
+        const appointments= await appointmentModel.find({docId})
+        let earnings= 0;
+        appointments.map((item)=>{
+
+            if(item.isCompleted || item.payment){
+                earnings += item.amount;
+            }
+
+        })
+        let patients = []
+
+        appointments.map((item)=>{
+            if(patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+
+        const dashData= {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0, 5)
+        }
+        res.json({success:true, dashData})
+
+    }catch(err){
+        console.log(err.message)
+        res.json({success: false, message: err.message })
+    }
+}
+
+//api to get doctor profile for doctor pane
+const doctorProfile= async(req, res)=>{
+    try {
+        const {docId}= req.body
+        const profileData= await doctorModel.findById(docId).select('-password')
+
+        res.json({success:true, profileData});
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})   
+    }
+}
+
+//api to update doctor profile
+
+const updatedocProfile= async(req,res)=>{
+    try {
+        
+        const{docId, fees, address, available}=req.body
+        await doctorModel.findByIdAndUpdate(docId, {fees, address, available})
+
+        res.json({success:true, message:"Profile Updated"})
+    } catch (error) {
+       console.log(error)
+        res.json({success:false, message:error.message})   
+    }
+}
+export {changeAvailablity,updatedocProfile, doctorProfile, doctorDashboard, appointmentsDoctor, loginDoctor, doctorList, appointmentCancel, appointmentComplete}
--- backend/routes/doctorroute.js
+++ backend/routes/doctorroute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { appointmentComplete, appointmentsDoctor, doctorList, loginDoctor } from '../controllers/doctorcontroller.js'
+import { appointmentComplete, appointmentsDoctor, doctorDashboard, doctorList, loginDoctor, updatedocProfile } from '../controllers/doctorcontroller.js'
 import authDoctor from '../middlewares/authDoctor.js'
 import { appointmentCancel } from '../controllers/admincontroller.js'
 
@@ -14,5 +14,11 @@ doctorRouter.get('/appointments', authDoctor, appointmentsDoctor )
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete)
 
 doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel)
+
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard)
+
+doctorRouter.get('/profile', authDoctor, doctorProfile)
+
+doctorRouter.post('/update-profile', authDoctor, updatedocProfile)
 
 export default doctorRouter
